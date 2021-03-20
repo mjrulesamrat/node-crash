613b9505226d45a1af769adf85ebeb4949ee7289
Jay Modi <mjrulesamrat@gmail.com>
2021-03-21 01:56:57 +0530

    Node globals

---
+++ 1-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS
+
+// __dirname  - path to current directory
+// __filename - filename
+// require    - function to use modules (CommonJS)
+// module     - infor about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname);
+
+setInterval(()=>{
+    console.log('Hello World!')
+}, 1000)
