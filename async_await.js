ae3e3763cd687ba929e885603ecbb9e4983dda7c
Jay Modi <mjrulesamrat@gmail.com>
2021-03-27 11:49:41 +0530

    Direct fs Read/Write promise functions

--- async_await.js
+++ async_await.js
@@ -1,19 +1,13 @@
 // async await demo to read and write to files
-const {readFile, writeFile} = require('fs')
-
-// simplify the promise function with builtins
-const util = require('util')
-
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile)
+const {readFile, writeFile} = require('fs').promises
 
 const start = async() => {
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf8')
-        const second = await readFilePromise('./content/second.txt', 'utf8')
-        await writeFilePromise(
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile(
             './content/result-mind-boom.txt',
-            `This is awesome: ${first} and ${second}`
+            `This is awesome, legendary: ${first} and ${second}`
             )
         console.log(first)
         console.log(second)
